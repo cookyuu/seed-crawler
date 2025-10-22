251e063b8da0843162e2ffc264b2c7aa76eef30f
임해규 <limk9500@gmail.com>
2025-10-22 16:56:05 +0900

    setting: init seed-crawler core

---
+++ core/src/test/java/com/seed_crawler/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seed_crawler.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
