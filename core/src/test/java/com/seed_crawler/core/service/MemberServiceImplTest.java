5de6aa7399e54a8a0a5886233f36ebed37b9b38e
임해규 <limk9500@gmail.com>
2025-11-20 17:02:39 +0900

    rename: Test 파일 네이밍 수정

--- core/src/test/java/com/seed_crawler/core/service/MemberServiceImplTest.java
+++ core/src/test/java/com/seed_crawler/core/service/MemberServiceImplTest.java
@@ -19,7 +19,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-class MemberDtoServiceImplTest {
+class MemberServiceImplTest {
 
     @Mock
     MemberRepository memberRepository;
--- core/src/test/java/com/seed_crawler/core/validator/MemberValidatorTest.java
+++ core/src/test/java/com/seed_crawler/core/validator/MemberValidatorTest.java
@@ -4,9 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
-class MemberDtoValidatorTest {
+class MemberValidatorTest {
 
     MemberValidator v = new MemberValidator();
 
